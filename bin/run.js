cde18079766aa86338ea62314df21a628f499454
jp <jpso.guerreiro@gmail.com>
2021-02-13 12:23:45 -0300

    changerun.js

--- bin/run.js
+++ bin/run.js
@@ -5,6 +5,7 @@ import chokidar from "chokidar"
 import spawner from "./../src/run-tool.js"
 import index from "./../src/index.js"
 console.log("Spawning git commit...")
+
 spawner("git add *", ()=>{
 	spawner("git commit -m \"Gitwatcher_initial_commit\"", (code)=>{
 		console.log("Git spawner exited	with "+code)
